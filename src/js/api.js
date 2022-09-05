01bfaf585832684bf3bb151c7d8743fd10de840b
Bianca Vellego <biancavell@hotmail.com>
2022-09-04 23:11:43 -0300

    chore: creating html structure. Items added: header, nav bar and  background video.

---
+++ src/js/api.js
@@ -0,0 +1,116 @@
+export class Api {
+
+    static ulrBase = "http://localhost:6278/";
+
+    static login() {
+    
+        
+    
+    }
+
+    static createUser() {
+    
+    
+    
+    }
+
+    static listCompanies() {
+    
+    
+    
+    }
+
+    static listCompaniesBySector() {
+    
+
+    
+    }
+
+
+    //Employees (logged users):
+    static listAllSectorEmployees() {
+    
+    
+    
+    }
+
+    static listEmployeeDepartment() {
+    
+    
+    
+    }
+
+    static updateEmployeeData() {
+    
+    
+    
+    }
+
+    //Admin
+    static listAllEmployees() {
+    
+        
+
+    }
+
+    static noDepartmentUsers() {
+    
+        
+    
+    }
+
+    static updateEmployeeDataAdmin() {
+    
+        
+    
+    }
+
+    static subscribeCompany() {
+    
+
+    
+    }
+
+    static listAllSectors() {
+    
+        
+        
+    }
+
+    static listAllDepartments() {
+    
+        
+    
+    }
+
+    static listAllCompaniesDepartments() {
+        
+
+    
+    }
+
+    static createDepartment() {
+    
+    
+
+    }
+
+    static hireEmployee() {
+    
+        
+
+    }
+
+    static fireEmployee() {
+    
+        
+    
+    }
+
+    static deleteDepartment() {
+    
+    
+    
+    }
+
+}
