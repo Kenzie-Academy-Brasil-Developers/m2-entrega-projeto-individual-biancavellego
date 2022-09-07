41fcda1bd30e1d415d6620c32f84b9b2eb2db511
Bianca Vellego <biancavell@hotmail.com>
2022-09-06 22:54:10 -0300

    feat: adding structured home page + functioning light/dark mode + mobile/desktop responsivity.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,1 +1,24 @@
 import { Api } from "../../js/api.js";
+
+export class HomePage {
+
+    static handleDarkMode() {
+        
+        const modeButton = document.querySelectorAll("#mode__button");
+        const html       = document.querySelector("html");
+
+        modeButton.forEach(element => {
+        
+            element.addEventListener("click", () => {
+            
+                html.classList.toggle("dark__mode");
+            
+            });
+        });
+    }
+
+    
+
+}
+
+HomePage.handleDarkMode()
