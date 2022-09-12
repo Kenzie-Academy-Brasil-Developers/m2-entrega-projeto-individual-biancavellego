cc14c165297853f2edb2acc215d79034dcd4238c
Bianca Vellego <biancavell@hotmail.com>
2022-09-12 13:28:14 -0300

    feat: dashboard responsive design for mobile + desktop complete. Page is also hiding and revealing navigation menus.

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -14,7 +14,8 @@ export class Dashboard {
         }
         if(this.isAdmin) {
         
-            //render additional admin resources...
+            const adminOptions = document.querySelector(".admin");
+            adminOptions.classList.remove("admin");
         
         }
     }
@@ -32,7 +33,82 @@ export class Dashboard {
     
     }
 
+    static handleProfileMenu() {
+    
+        const profileButton = document.querySelector(".nav__menu");
+        const profileMenu   = document.querySelector(".nav__button__container");
+
+        profileButton.addEventListener("click", () => {
+        
+            profileMenu.classList.toggle("hidden");
+        
+        });
+    }
+
+    static handleProfileMenuOptions() {
+
+        const companyButton  = document.querySelector(".company__button");
+        const companyOptions = document.querySelector(".company__button__options");
+
+        companyButton.addEventListener("click", () => {
+        
+            companyOptions.classList.toggle("hiddenOptions");
+        
+        });
+
+        const sectorsButton  = document.querySelector(".sectors__button");
+        const sectorsOptions = document.querySelector(".sectors__button__options");
+
+        sectorsButton.addEventListener("click", () => {
+        
+            sectorsOptions.classList.toggle("hiddenOptions");
+        
+        });
+
+        const departmentButton  = document.querySelector(".department__button");
+        const departmentOptions = document.querySelector(".department__button__options");
+
+        departmentButton.addEventListener("click", () => {
+        
+            departmentOptions.classList.toggle("hiddenOptions");
+        
+        });
+    }
+
+    static handleDesktopMenu() {
+    
+        const companyButton  = document.querySelector(".company__button--desktop");
+        const companyOptions = document.querySelector(".company__button__options--desktop");
+
+        companyButton.addEventListener("click", () => {
+        
+            companyOptions.classList.toggle("hidden");
+        
+        });
+
+        const sectorsButton = document.querySelector(".sectors__button--desktop");
+        const sectorsOptions = document.querySelector(".sectors__button__options--desktop");
+
+        sectorsButton.addEventListener("click", () => {
+        
+            sectorsOptions.classList.toggle("hidden");
+        
+        });
+
+        const departmentButton = document.querySelector(".department__button--desktop");
+        const departmentOptions = document.querySelector(".department__button__options--desktop");
+
+        departmentButton.addEventListener("click", () => {
+        
+            departmentOptions.classList.toggle("hidden");
+        
+        });
+    }
+
 }
 
 Dashboard.renderDashboard();
 Dashboard.handleDarkMode();
+Dashboard.handleProfileMenu();
+Dashboard.handleProfileMenuOptions();
+Dashboard.handleDesktopMenu();
